6254e028fac88d3b3c97a3d5bc975b9d53e36a8b
sahilsh23ah <sahilshah4447@gmail.com>
2024-02-05 22:09:03 +0530

    Added all files

---
+++ script.js
@@ -0,0 +1,68 @@
+// 4 piller of DOM 
+
+// 1. selection of Elements 
+
+// let a = document.querySelector("h1");
+// console.log(a);
+
+
+// 2. Changing HTML
+
+// let a = document.querySelector("h1");
+// a.innerHTML = "hihihi"
+
+
+// 3. Changing CSS
+
+// let a = document.querySelector("h1");
+// a.style.color = "blue";
+// a.style.backgroundColor = "black";
+
+
+// 4. Event listener
+
+// let a = document.querySelector("h1");
+// a.addEventListener('click',function(){
+//     a.innerHTML = "Change HTML";
+//     a.style.color = "Yellow";
+//     a.style.backgroundColor = "black";
+// });
+
+
+// ********** pratice ***********
+
+// let bulb = document.querySelector("#bulb");
+// let btn = document.querySelector("button");
+
+// btn.addEventListener('click',function(){
+//     bulb.style.backgroundColor = "yellow";
+//     btn.innerHTML = "OFF";
+// });
+
+// let flag = 0;
+// btn.addEventListener('click',function(){
+//     if(flag == 0){
+//         bulb.style.backgroundColor = "yellow";
+//         btn.innerHTML = "OFF";
+//         console.log("clicked");
+//         flag = 1;
+//     }else{
+//         bulb.style.backgroundColor = "transparent";
+//         console.log(" again clicked");
+//         btn.innerHTML = "ON";
+//         flag = 0;
+//     }
+// });
+
+// let h = document.querySelectorAll("h2");
+// console.log(h);
+// h.forEach(function(e){
+//     console.log(e);
+// });
+
+
+// let h = document.querySelector("h2");
+// h.innerHTML = "<h5>Hello</h5>";
+// h.textContent = "<h5>Hello</h5>";
+
+
